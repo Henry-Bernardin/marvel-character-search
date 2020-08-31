ba9962c699c840775cb8a1a8b05251ef5b9c99fd
Henry-Bernardin <henry.bernardin@gmail.com>
2020-08-31 18:24:34 -0400

    imported axios to app.js

--- marvel-character-search/src/App.js
+++ marvel-character-search/src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 import './App.css';
 import Header from './components/Header';
 
@@ -6,6 +7,9 @@ const App = () => {
   const [characters, setCharacters] = useState([])
   const [isLoading, setIsLoading] = useState(true)
   
+ 
+
+
   return ( 
   <div className="container"> 
   <Header />
