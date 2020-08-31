bb36eae8f5caf2425e86cfccb24e7f4de1f36f6e
Henry-Bernardin <henry.bernardin@gmail.com>
2020-08-31 18:22:44 -0400

    Created isLoading, setIsLoading, characters, & setCharacters const

--- marvel-character-search/src/App.js
+++ marvel-character-search/src/App.js
@@ -3,9 +3,14 @@ import './App.css';
 import Header from './components/Header';
 
 const App = () => {
-  return <div className="container"> 
+  const [characters, setCharacters] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  
+  return ( 
+  <div className="container"> 
   <Header />
   </div>
+  )
 }
 
 export default App;
