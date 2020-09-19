7cadc958b7f34e241b897974efb380796e72f6bd
Henry-Bernardin <henry.bernardin@gmail.com>
2020-09-18 23:30:02 -0400

    added more to comment

--- marvel-character-search/src/App.js
+++ marvel-character-search/src/App.js
@@ -12,7 +12,7 @@ useEffect(() => {
     const result = await axios(
       `http(s)://gateway.marvel.com/characters`
     )
-//Fetch the api
+//Fetch the api with its key
     console.log(result.data)
   }
 
